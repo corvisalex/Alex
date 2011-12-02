0099b163d24a146aca2845c3d6da242f3936cb9f
Alexander Vasiliev <night_devil_2006@mail.ru>
2011-12-03 05:34:02 +0600

    First shit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alex::Application.config.secret_token = '6bc208fb09cce9e0fd51de58d91df1fbd27e67cdb71d1d6600464d559de5201daa4b6ecd58796ef3bf721a44c3db56ac6e03e4bbb091255a658c6641760964ec'
